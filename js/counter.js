65e6ad02fd11db3f2c037bd1f7deceb487b1a56a
WagnerSantos98 <wagner.jesus98@outlook.com>
2021-11-08 00:49:14 -0300

    projeto bioterio

--- js/counter.js
+++ js/counter.js
@@ -11,3 +11,21 @@ function Soma(){
   });
 }
 
+//Perfil
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+//Fechar o menu
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
